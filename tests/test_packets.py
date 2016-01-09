c29761dba36c84cbd6109a7f2db143c82f959344
Julien Lirochon <julien@lirochon.net>
2016-01-09 19:18:59 +0100

    fixes test_decode_unconnected_ping()

--- tests/test_packets.py
+++ tests/test_packets.py
@@ -37,9 +37,9 @@ def test_decode_unconnected_ping():
         b"\xfd\xfd\xfd\xfd\x12\x34\x56\x78\x00\x05\x27\x00\xaa\x0a\x23\xa3"
     )
     packet = packets.UnconnectedPing(data)
-    assert packet.ping_id == 193351
+    assert packet.time == 193351
     assert packet.client_guid == 1450258689827747
-    assert six.text_type(packet) == "UnconnectedPing(ping_id=193351, client_guid=1450258689827747)"
+    assert six.text_type(packet) == "UnconnectedPing(time=193351, client_guid=1450258689827747)"
 
 
 def test_decode_packet():
